8dba80c55e463cc3cad3be143a629ca50666bbba
Aironys Garrido <aironys1@hotmail.com>
2022-03-15 01:15:21 -0300

    Projeto soma e subtração.

---
+++ curso-01.js
@@ -0,0 +1,38 @@
+alert('Click em "OK" para visualizar o projeto ')
+
+let somar = function(valor1, valor2){
+    let result = valor1 + valor2;
+    console.log(result);
+}
+
+
+somar(4, 1);
+somar(10, 13);
+somar(34, 24);
+
+
+
+
+let subtrair = function(valor1, valor2, valor3){
+    let result = valor1 - valor2 - valor3;
+    console.log(result);
+}
+
+subtrair(2,5,1);
+subtrair(10,5,8);
+subtrair(100,25,2);
+
+
+
+let multiplicar = function(valor1, valor2 ){
+    console.log(valor1 * valor2)
+    return valor1 * valor2;
+
+}
+
+let somar2 = function(valor1, valor2){
+    return valor1 + valor2;
+}
+
+let resultado = somar2(10,10);
+
